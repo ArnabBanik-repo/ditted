c7e2f3b4e0b4f197a86c8e8d4ec00786d6704551
Arnab Banik <arnabbanik9@gmail.com>
2024-01-27 16:55:46 +0530

    remove .env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       </div>
       <div className="border shadow py-3 px-2">
         <TopicCreateForm />
-        <Divider className="my-2"/>
+        <Divider className="my-2" />
         <h3 className="text-lg">Topics</h3>
         <TopicList />
       </div>
